225b42abfeb3164560979d4dd5f357143bae78ef
aksharma2820 <aksharma.2820@gmail.com>
2016-03-30 14:19:25 +0700

    Added files via upload

---
+++ startnp.py
@@ -0,0 +1,32 @@
+from PIL import Image
+import ImageGrab
+import os
+import numpy as np
+
+bbox = (714,149,721,156)
+sample  = "C:/Users/vssharma/Pictures/Screenshots/Screenshot (30).png" 
+bg = Image.open(sample)
+sampletag = bg.crop(bbox)
+temp = np.array(sampletag)
+tagarraypixel = temp[:,:,:3]
+
+testArea = ()     #pixel values to be cropped 
+while True:
+    current  = ImageGrab.grab()
+    filename = os.getcwd()+'\\image.png'        #save filename 
+    current.save(filename, 'PNG')
+    fg = Image.open(filename)                   ##foreground
+    currentTag  = fg.crop(bbox)
+    currentarraypixel = np.array(currentTag)
+
+    print tagarraypixel
+    print currentarraypixel
+    currentTag.show()
+    current.show()
+    if np.sum(tagarraypixel - currentarraypixel) == 0:
+        target = current.crop(testArea)          #another bounding box which crops the image part needed(the test result on top of the stack)
+        
+    else:
+        pass
+
+
